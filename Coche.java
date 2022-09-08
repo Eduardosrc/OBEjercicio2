74756da62ece90cc1e8cb58da4ddeb500af33598
Eduardosrc <102379404+Eduardosrc@users.noreply.github.com>
2022-09-07 22:38:28 -0500

    Add files via upload

---
+++ Coche.java
@@ -0,0 +1,12 @@
+public class Coche{
+    private int puertas = 0;
+
+    public void incrementarPuerta(){
+        this.puertas++;
+    }
+
+    public int verPuertas(){
+        return this.puertas;
+    }
+
+}
---
+++ Ejercicio.java
@@ -0,0 +1,24 @@
+public class Ejercicio {
+
+    public static void main(String[] args) {
+        
+        sumar(5, 5, 5);
+    }
+
+    public static void sumar(int num1, int num2, int num3){
+        int resultado = num1 + num2 + num3;
+        System.out.println("Resultado de la suma: " + resultado);
+        System.out.println();
+
+        Coche c = new Coche();
+
+        c.incrementarPuerta();
+        c.incrementarPuerta();
+        c.incrementarPuerta();
+
+        System.out.println("Número de puertas: " + c.verPuertas());
+    }
+    
+}
+
+
